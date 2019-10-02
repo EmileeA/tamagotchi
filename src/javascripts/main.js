1c6f67610b5f2c19d4cef9d992b2c53f9e22a60b
Emilee Mitchell <emcakes@gmail.com>
2019-10-02 18:27:59 -0500

    built my quads

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,14 @@
 import '../styles/main.scss';
 import eat from './components/eat.js';
+import play from './components/play.js';
+import fight from './components/fight.js';
+import sleep from './components/sleep.js';
 
 const init = () => {
   eat.printEat();
+  play.printPlay();
+  fight.printFight();
+  sleep.printSleep();
 };
 
 init();
